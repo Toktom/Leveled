b04762ab2620f66dcdb207dcd94f46ae11bd8f7d
Michael Markus Ackermann <michael_ackermann@outlook.com>
2021-03-11 10:23:18 -0300

    Added frist farming and fishing events

--- src/main/java/io/github/toktom/skills/list/farming/FarmingSkillEventHandler.java
+++ src/main/java/io/github/toktom/skills/list/farming/FarmingSkillEventHandler.java
@@ -2,6 +2,11 @@ package io.github.toktom.skills.list.farming;
 
 import io.github.toktom.skills.Skill;
 import io.github.toktom.skills.SkillsEventHandler;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class FarmingSkillEventHandler extends SkillsEventHandler
 {
@@ -9,5 +14,33 @@ public class FarmingSkillEventHandler extends SkillsEventHandler
 	public FarmingSkillEventHandler(Skill skill)
 	{
 		super(skill);
+	}
+
+	private void onBoneMeal(BonemealEvent event)
+	{
+		PlayerEntity player = event.getPlayer();
+		if (this.getLevel(player) >= 1)
+		{
+			this.addExp(player, 10);
+		} else
+		{
+			if (!event.getWorld().isClientSide())
+			{
+				event.getWorld().playSound((PlayerEntity) null, event.getPos(), SoundEvents.ANVIL_LAND,
+						SoundCategory.BLOCKS, 1.0F, 1f);
+				lowLevelMessage(player);
+			}
+			event.setCanceled(true);
+		}
+	}
+
+	@SuppressWarnings("resource")
+	@SubscribeEvent
+	public void farmingOnBoneMealUse(BonemealEvent event)
+	{
+		if (!event.getWorld().isClientSide)
+		{
+			onBoneMeal(event);
+		}
 	}
 }
--- src/main/java/io/github/toktom/skills/list/fishing/FishingSkillEventHandler.java
+++ src/main/java/io/github/toktom/skills/list/fishing/FishingSkillEventHandler.java
@@ -2,6 +2,15 @@ package io.github.toktom.skills.list.fishing;
 
 import io.github.toktom.skills.Skill;
 import io.github.toktom.skills.SkillsEventHandler;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.ItemFishedEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.EventAccessTransformer;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class FishingSkillEventHandler extends SkillsEventHandler
 {
@@ -10,4 +19,46 @@ public class FishingSkillEventHandler extends SkillsEventHandler
 	{
 		super(skill);
 	}
+
+	// TODO: Fix this shit
+	private void onFishedEvent(ItemFishedEvent event, Item fish, long level, long exp)
+	{
+		World world = event.getPlayer().getCommandSenderWorld();
+		PlayerEntity player = event.getPlayer();
+		if (this.getLevel(player) >= level)
+		{
+			if (event.getDrops().equals(fish))
+			{
+				this.addExp(player, exp);
+			} else
+			{
+				if (!world.isClientSide)
+				{
+					event.setCanceled(true);
+					this.lowLevelMessage(player);
+				}
+			}
+		} else
+		{
+			if (!world.isClientSide)
+			{
+				event.setCanceled(true);
+				this.lowLevelMessage(player);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void fishingFishCatchedEvent(ItemFishedEvent event)
+	{
+		if (!event.getPlayer().getCommandSenderWorld().isClientSide)
+		{
+			onFishedEvent(event, Items.COD, 1, 40);
+			onFishedEvent(event, Items.SALMON, 1, 40);
+			onFishedEvent(event, Items.PUFFERFISH, 1, 40);
+			onFishedEvent(event, Items.TROPICAL_FISH, 1, 40);
+			onFishedEvent(event, Items.COD, 1, 40);
+		}
+	}
+
 }
